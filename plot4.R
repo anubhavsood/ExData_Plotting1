f32fd965add5fc2d607107693a0cb1e0678152f9
Anubhav <anubhav.sood@yahoo.com.au>
2015-03-08 06:01:43 +1100

    Update Plot4.r file

--- plot4.R
+++ plot4.R
@@ -8,11 +8,26 @@ x<-function(x1, x2){as.POSIXct(paste(x1, x2), format = "%d/%m/%Y %H:%M:%S")}
 sample$datetime <- mapply(x,sample$Date,sample$Time)
 
 png(file = paste(getwd(),"/plot4.png",sep=""),width=480,height=480,units="px")
+par(mfrow = c(2,2))
+
+plot(sample$datetime, sample$Global_active_power, type="l", xaxt = "n",
+     ylab = "Global Active Power", xlab = "")
+axis.POSIXct(1, as.POSIXct(sample$datetime, origin="1970-01-01"))
+
+plot(sample$datetime, sample$Voltage, type="l", xaxt = "n",
+     ylab = "Voltage", xlab = "datetime")
+axis.POSIXct(1, as.POSIXct(sample$datetime, origin="1970-01-01"))
+
 plot(sample$datetime, sample$Sub_metering_1, type="l", xaxt = "n",
      ylab = "Energy sub metering", xlab = "")
 lines(sample$datetime, sample$Sub_metering_2, col = "red")
 lines(sample$datetime, sample$Sub_metering_3, col = "blue")
 axis.POSIXct(1, as.POSIXct(sample$datetime, origin="1970-01-01"))
 legend("topright", lty = 1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       bty = "n")
+
+plot(sample$datetime, sample$Global_reactive_power, type="l", xaxt = "n",
+     ylab = "Global_reactive_power", xlab = "datetime")
+axis.POSIXct(1, as.POSIXct(sample$datetime, origin="1970-01-01"))
 dev.off()
